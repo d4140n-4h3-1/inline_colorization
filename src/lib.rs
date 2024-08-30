26c57652942e031b5dd11a86059d787ea0a488ff
d4140n-4h3-1 <55861113+d4140n-4h3-1@users.noreply.github.com>
2024-08-29 20:19:19 -0500

    Replace file via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,47 @@
-#[allow(non_upper_case_globals)]
-pub const style_bold: &str = "\x1B[1m";
-// #[allow(non_upper_case_globals)]
-// pub const style_un_bold: &str = "\x1B[21m";
-#[allow(non_upper_case_globals)]
-pub const style_underline: &str = "\x1B[4m";
-// #[allow(non_upper_case_globals)]
-// pub const style_un_underline: &str = "\x1B[24m";
-#[allow(non_upper_case_globals)]
-pub const style_reset: &str = "\x1B[0m";
+#[allow(non_upper_case_globals)] pub const style_bold: &str = "\x1B[1m";
+#[allow(non_upper_case_globals)] pub const style_underline: &str = "\x1B[4m";
+#[allow(non_upper_case_globals)] pub const style_italics: &str = "\x1B[3m";
+#[allow(non_upper_case_globals)] pub const style_blink: &str = "\x1b[5m";
+#[allow(non_upper_case_globals)] pub const style_reset: &str = "\x1B[0m";
 
-#[allow(non_upper_case_globals)]
-pub const color_black: &str = "\x1B[30m";
-#[allow(non_upper_case_globals)]
-pub const color_red: &str = "\x1B[31m";
-#[allow(non_upper_case_globals)]
-pub const color_green: &str = "\x1B[32m";
-#[allow(non_upper_case_globals)]
-pub const color_yellow: &str = "\x1B[33m";
-#[allow(non_upper_case_globals)]
-pub const color_blue: &str = "\x1B[34m";
-#[allow(non_upper_case_globals)]
-pub const color_magenta: &str = "\x1B[35m";
-#[allow(non_upper_case_globals)]
-pub const color_cyan: &str = "\x1B[36m";
-#[allow(non_upper_case_globals)]
-pub const color_white: &str = "\x1B[37m";
-#[allow(non_upper_case_globals)]
-pub const color_bright_black: &str = "\x1B[90m";
-#[allow(non_upper_case_globals)]
-pub const color_bright_red: &str = "\x1B[91m";
-#[allow(non_upper_case_globals)]
-pub const color_bright_green: &str = "\x1B[92m";
-#[allow(non_upper_case_globals)]
-pub const color_bright_yellow: &str = "\x1B[93m";
-#[allow(non_upper_case_globals)]
-pub const color_bright_blue: &str = "\x1B[94m";
-#[allow(non_upper_case_globals)]
-pub const color_bright_magenta: &str = "\x1B[95m";
-#[allow(non_upper_case_globals)]
-pub const color_bright_cyan: &str = "\x1B[96m";
-#[allow(non_upper_case_globals)]
-pub const color_bright_white: &str = "\x1B[97m";
-#[allow(non_upper_case_globals)]
-pub const color_reset: &str = "\x1B[39m";
+#[allow(non_upper_case_globals)] pub const rgb: &str = "\x1C[10m";
 
-#[allow(non_upper_case_globals)]
-pub const bg_black: &str = "\x1B[40m";
-#[allow(non_upper_case_globals)]
-pub const bg_red: &str = "\x1B[41m";
-#[allow(non_upper_case_globals)]
-pub const bg_green: &str = "\x1B[42m";
-#[allow(non_upper_case_globals)]
-pub const bg_yellow: &str = "\x1B[43m";
-#[allow(non_upper_case_globals)]
-pub const bg_blue: &str = "\x1B[44m";
-#[allow(non_upper_case_globals)]
-pub const bg_magenta: &str = "\x1B[45m";
-#[allow(non_upper_case_globals)]
-pub const bg_cyan: &str = "\x1B[46m";
-#[allow(non_upper_case_globals)]
-pub const bg_white: &str = "\x1B[47m";
-#[allow(non_upper_case_globals)]
-pub const bg_bright_black: &str = "\x1B[100m";
-#[allow(non_upper_case_globals)]
-pub const bg_bright_red: &str = "\x1B[101m";
-#[allow(non_upper_case_globals)]
-pub const bg_bright_green: &str = "\x1B[102m";
-#[allow(non_upper_case_globals)]
-pub const bg_bright_yellow: &str = "\x1B[103m";
-#[allow(non_upper_case_globals)]
-pub const bg_bright_blue: &str = "\x1B[104m";
-#[allow(non_upper_case_globals)]
-pub const bg_bright_magenta: &str = "\x1B[105m";
-#[allow(non_upper_case_globals)]
-pub const bg_bright_cyan: &str = "\x1B[106m";
-#[allow(non_upper_case_globals)]
-pub const bg_bright_white: &str = "\x1B[107m";
-#[allow(non_upper_case_globals)]
-pub const bg_reset: &str = "\x1B[49m";
+#[allow(non_upper_case_globals)] pub const fg_black: &str = "\x1B[30m";
+#[allow(non_upper_case_globals)] pub const fg_red: &str = "\x1B[31m";
+#[allow(non_upper_case_globals)] pub const fg_green: &str = "\x1B[32m";
+#[allow(non_upper_case_globals)] pub const fg_yellow: &str = "\x1B[33m";
+#[allow(non_upper_case_globals)] pub const fg_blue: &str = "\x1B[34m";
+#[allow(non_upper_case_globals)] pub const fg_magenta: &str = "\x1B[35m";
+#[allow(non_upper_case_globals)] pub const fg_cyan: &str = "\x1B[36m";
+#[allow(non_upper_case_globals)] pub const fg_white: &str = "\x1B[37m";
+#[allow(non_upper_case_globals)] pub const fg_b_black: &str = "\x1B[90m";
+#[allow(non_upper_case_globals)] pub const fg_b_red: &str = "\x1B[91m";
+#[allow(non_upper_case_globals)] pub const fg_b_green: &str = "\x1B[92m";
+#[allow(non_upper_case_globals)] pub const fg_b_yellow: &str = "\x1B[93m";
+#[allow(non_upper_case_globals)] pub const fg_b_blue: &str = "\x1B[94m";
+#[allow(non_upper_case_globals)] pub const fg_b_magenta: &str = "\x1B[95m";
+#[allow(non_upper_case_globals)] pub const fg_b_cyan: &str = "\x1B[96m";
+#[allow(non_upper_case_globals)] pub const fg_b_white: &str = "\x1B[97m";
+#[allow(non_upper_case_globals)] pub const fg_reset: &str = "\x1B[39m";
+
+#[allow(non_upper_case_globals)] pub const bg_black: &str = "\x1B[40m";
+#[allow(non_upper_case_globals)] pub const bg_red: &str = "\x1B[41m";
+#[allow(non_upper_case_globals)] pub const bg_green: &str = "\x1B[42m";
+#[allow(non_upper_case_globals)] pub const bg_yellow: &str = "\x1B[43m";
+#[allow(non_upper_case_globals)] pub const bg_blue: &str = "\x1B[44m";
+#[allow(non_upper_case_globals)] pub const bg_magenta: &str = "\x1B[45m"; 
+#[allow(non_upper_case_globals)] pub const bg_cyan: &str = "\x1B[46m"; 
+#[allow(non_upper_case_globals)] pub const bg_white: &str = "\x1B[47m";
+#[allow(non_upper_case_globals)] pub const bg_b_black: &str = "\x1B[100m";
+#[allow(non_upper_case_globals)] pub const bg_b_red: &str = "\x1B[101m";
+#[allow(non_upper_case_globals)] pub const bg_b_green: &str = "\x1B[102m";
+#[allow(non_upper_case_globals)] pub const bg_b_yellow: &str = "\x1B[103m";
+#[allow(non_upper_case_globals)] pub const bg_b_blue: &str = "\x1B[104m";
+#[allow(non_upper_case_globals)] pub const bg_b_magenta: &str = "\x1B[105m";
+#[allow(non_upper_case_globals)] pub const bg_b_cyan: &str = "\x1B[106m";
+#[allow(non_upper_case_globals)] pub const bg_b_white: &str = "\x1B[107m";
+#[allow(non_upper_case_globals)] pub const bg_reset: &str = "\x1B[49m";
+
+#[allow(non_upper_case_globals)] pub const quote: &str = "\u{0022}";
+#[allow(non_upper_case_globals)] pub const apost: &str = "\u{0027}";
+#[allow(non_upper_case_globals)] pub const backspace: &str = "\x08";
